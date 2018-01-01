1d6623e390d6c0de2629e2b72f2dec684d3cb159
Ross Edlin <rossedlin@gmail.com>
2018-01-01 13:46:46 +0000

    update tot github + folders

--- build/siteground-live.sh
+++ build/siteground-live.sh
@@ -6,7 +6,7 @@ rm -R -f old_projects
 rm -R -f projects.rossedlin.com
 
 #Git
-git clone -b master https://bitbucket.org/rossedlin/projects.rossedlin.com
+git clone -b master https://github.com/rossedlin/projects.rossedlin.com
 
 if [ -d ~/rossedlin.com/projects.rossedlin.com ]; then
 
@@ -14,22 +14,18 @@ if [ -d ~/rossedlin.com/projects.rossedlin.com ]; then
     cd ~/rossedlin.com/projects.rossedlin.com
     composer install
     composer update
-#
-#    #Environment File
-#    cd ~/rossedlin.com/
-#    cp ~/rossedlin.com/www/.env ~/rossedlin.com/www.rossedlin.com-laravel/.env
-#
-#    #Folders
-#    cd ~/rossedlin.com/
-#    mv www old_www
-#    mv www.rossedlin.com-laravel www
-#
+
+    #Folders
+    cd ~/rossedlin.com/
+    mv projects old_projects
+    mv projects.rossedlin.com projects
+
 #    #Public
 #    cd ~/rossedlin.com/www
 #    cp ./build/live/robots.txt ./public/robots.txt
 #    cp ./build/live/.htaccess ./public/.htaccess
-#
-#    #Permissions
-#    chmod 755 -R ~/rossedlin.com/www
+
+    #Permissions
+    chmod 755 -R ~/rossedlin.com/projects
 
 fi
