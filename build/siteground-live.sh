b0c625459856d93ba4b854f59e1a1853ad59e382
Ross Edlin <rossedlin@gmail.com>
2018-01-01 13:40:03 +0000

    add - site ground build script

---
+++ build/siteground-live.sh
@@ -0,0 +1,35 @@
+cd ~/rossedlin.com/
+echo "Install -> projects.rossedlin.com"
+
+#Cleanup
+rm -R -f old_www
+rm -R -f www.rossedlin.com-laravel
+
+#Git
+git clone -b master https://bitbucket.org/rossedlin/projects.rossedlin.com
+
+if [ -d ~/rossedlin.com/projects.rossedlin.com ]; then
+
+    #Composer
+    cd ~/rossedlin.com/projects.rossedlin.com
+    composer install
+    composer update
+#
+#    #Environment File
+#    cd ~/rossedlin.com/
+#    cp ~/rossedlin.com/www/.env ~/rossedlin.com/www.rossedlin.com-laravel/.env
+#
+#    #Folders
+#    cd ~/rossedlin.com/
+#    mv www old_www
+#    mv www.rossedlin.com-laravel www
+#
+#    #Public
+#    cd ~/rossedlin.com/www
+#    cp ./build/live/robots.txt ./public/robots.txt
+#    cp ./build/live/.htaccess ./public/.htaccess
+#
+#    #Permissions
+#    chmod 755 -R ~/rossedlin.com/www
+
+fi
