f58c6c3a1ab95072f97a0ca233fd47d770c31c5d
Ross Edlin <rossedlin@gmail.com>
2018-01-01 14:39:09 +0000

    huge update to projects

---
+++ app/Project.php
@@ -0,0 +1,19 @@
+<?php
+namespace App;
+
+/**
+ * Created by PhpStorm.
+ *
+ * @author  Ross Edlin <contact@rossedlin.com>
+ *
+ * Date: 01/01/2018
+ * Time: 14:36
+ *
+ * Class Project
+ *
+ * @package App
+ */
+class Project
+{
+	
+}
