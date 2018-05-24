401db9f7c353c216aa44e0cbad95eb5adfd45d41
Ross Edlin <rossedlin@gmail.com>
2018-05-24 16:43:13 +0100

    huge cleanup for website front end

--- public/index.php
+++ public/index.php
@@ -2,237 +2,87 @@
 require __DIR__ . '/../vendor/autoload.php';
 ?>
 
-<!DOCTYPE html>
+
+<!doctype html>
 <html lang="en">
 <head>
-	<!-- Title -->
-	<title>Ross Edlin | Projects</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<!-- Required Meta Tags Always Come First -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Projects</title>
 
-	<!-- Favicon -->
-	<link rel="shortcut icon" href="http://cdn-aws-rossedlin.s3.eu-central-1.amazonaws.com/favicon/v1/favicon.ico">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-	<!-- Google Fonts -->
-	<link rel="stylesheet"
-		  href="//fonts.googleapis.com/css?family=PT+Sans%7CMerriweather%7COpen+Sans%3A300%2C400%2C600%2C700%2C800%7CPlayfair+Display%7CRoboto%7CRaleway%7CSpectral%7CRubik">
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
 
-	<!-- CSS Global Compulsory -->
-	<link rel="stylesheet" href="/assets/vendor/bootstrap/bootstrap.min.css">
-	<link rel="stylesheet" href="/assets/vendor/icon-awesome/css/font-awesome.min.css">
-	<link rel="stylesheet" href="/assets/vendor/icon-line-pro/style.css">
+        .full-height {
+            height: 100vh;
+        }
 
-	<!-- CSS Implementing Plugins -->
-	<link rel="stylesheet" href="/assets/vendor/icon-awesome/css/font-awesome.min.css">
-	<link rel="stylesheet" href="/assets/vendor/icon-hs/style.css">
-	<link rel="stylesheet" href="/assets/vendor/hamburgers/hamburgers.min.css">
-	<link rel="stylesheet" href="/assets/vendor/dzsparallaxer/dzsparallaxer.css">
-	<link rel="stylesheet" href="/assets/vendor/dzsparallaxer/dzsscroller/scroller.css">
-	<link rel="stylesheet" href="/assets/vendor/dzsparallaxer/advancedscroller/plugin.css">
-	<link rel="stylesheet" href="/assets/vendor/slick-carousel/slick/slick.css">
-	<link rel="stylesheet" href="/assets/vendor/cubeportfolio-full/cubeportfolio/css/cubeportfolio.min.css">
-	<link rel="stylesheet" href="/assets/vendor/fancybox/jquery.fancybox.css">
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
 
-	<!-- CSS Template -->
-	<link rel="stylesheet" href="assets/css/styles.op-consulting.css">
+        .position-ref {
+            position: relative;
+        }
 
-	<!-- Revolution Slider -->
-	<link rel="stylesheet"
-		  href="/assets/vendor/revolution-slider/revolution/fonts/pe-icon-7-stroke/css/pe-icon-7-stroke.css">
-	<link rel="stylesheet" href="/assets/vendor/revolution-slider/revolution/css/settings.css">
-	<link rel="stylesheet" href="/assets/vendor/revolution-slider/revolution/css/layers.css">
-	<link rel="stylesheet" href="/assets/vendor/revolution-slider/revolution/css/navigation.css">
-	<link rel="stylesheet" href="/assets/vendor/revolution-slider/revolution-addons/typewriter/css/typewriter.css">
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
 
-	<!-- CSS Customization -->
-	<link rel="stylesheet" href="/assets/css/custom.css">
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 12px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
 </head>
+<body>
+<div class="flex-center position-ref full-height">
 
-<body id="home-section" class="g-overflow-hidden">
-<main>
-	<!-- Header -->
-	<header id="js-header" class="u-header u-header--sticky-top u-header--change-appearance u-header--change-logo"
-			data-header-fix-moment="0">
-		<div class="u-header__section g-bg-black-opacity-0_2 g-transition-0_3 g-py-7 g-py-23--md"
-			 data-header-fix-moment-exclude="g-bg-black-opacity-0_5 g-py-23--md"
-			 data-header-fix-moment-classes="u-shadow-v19 g-bg-white g-py-15--md">
-			<nav class="navbar navbar-expand-lg g-py-0">
-				<div class="container g-pos-rel">
-					<!-- Logo -->
-					<a href="/" class="navbar-brand u-header__logo">
-						<img class="u-header__logo-img u-header__logo-img--main g-width-90" src="assets/img/logo.png"
-							 alt="Image description">
-						<img class="u-header__logo-img g-width-90" src="assets/img/logo-dark.png"
-							 alt="Image description">
-					</a>
-					<!-- End Logo -->
+    <div class="content">
+        <div class="title m-b-md">
+            Projects
+        </div>
 
-					<!-- Navigation -->
-					<div class="collapse navbar-collapse align-items-center flex-sm-row" id="navBar">
-						<ul id="js-scroll-nav"
-							class="navbar-nav text-uppercase g-letter-spacing-1 g-font-size-12 g-pt-20 g-pt-0--lg ml-auto">
-							<li class="nav-item g-mr-15--lg g-mb-7 g-mb-0--lg">
-								<a href="/" class="nav-link p-0">Home <span class="sr-only">(current)</span></a>
-							</li>
-							<li class="nav-item g-mx-15--lg g-mb-7 g-mb-0--lg">
-								<a href="https://www.rossedlin.com/contact/" target="_blank" class="nav-link p-0">Contact</a>
-							</li>
-						</ul>
-					</div>
-					<!-- End Navigation -->
-
-					<!-- Responsive Toggle Button -->
-					<button class="navbar-toggler btn g-line-height-1 g-brd-none g-pa-0 g-pos-abs g-top-15 g-right-0"
-							type="button"
-							aria-label="Toggle navigation"
-							aria-expanded="false"
-							aria-controls="navBar"
-							data-toggle="collapse"
-							data-target="#navBar">
-                <span class="hamburger hamburger--slider">
-                  <span class="hamburger-box">
-                    <span class="hamburger-inner"></span>
-                  </span>
-                </span>
-					</button>
-					<!-- End Responsive Toggle Button -->
-				</div>
-			</nav>
-		</div>
-	</header>
-	<!-- End Header -->
-
-	<div id="content" class="site-content">
-
-		<!-- Projects -->
-		<div class="container g-pt-50 g-pb-20 g-pt-140">
-			<!-- Heading -->
-			<div class="text-center mx-auto" style="margin-top: 60px;">
-				<h1 class="text-uppercase g-color-main-light-v1 g-font-weight-600 g-font-size-36 g-letter-spacing-2 mb-4">
-					Projects
-				</h1>
-				<p class="g-font-size-16">
-					Welcome to my projects site, this is a vanilla website utilising bootstrap.
-				</p>
-			</div>
-			<!-- End Heading -->
-		</div>
-		<!-- End Projects -->
-
-	</div>
-
-	<!-- Footer -->
-	<footer class="copyright g-py-20" style="background-color: #0a0a0a;">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-8 text-center text-md-left g-mb-10 g-mb-0--md">
-					<div class="d-lg-flex">
-						<small class="d-block g-font-size-default g-mr-10 g-mb-10 g-mb-0--md">
-							2017 - <?= date('Y') ?> © All Rights Reserved.
-						</small>
-					</div>
-				</div>
-
-				<div class="col-md-4 align-self-center">
-					<ul class="list-inline text-center text-md-right mb-0">
-
-						<li class="list-inline-item g-mx-10" data-toggle="tooltip" data-placement="top"
-							title="Facebook">
-							<a href="https://www.facebook.com/rossedlin" target="_blank"
-							   class="g-color-white-opacity-0_5 g-color-white--hover">
-								<i class="fa fa-facebook"></i>
-							</a>
-						</li>
-
-						<li class="list-inline-item g-mx-10" data-toggle="tooltip" data-placement="top"
-							title="Instagram">
-							<a href="https://www.instagram.com/rossedlin/" target="_blank"
-							   class="g-color-white-opacity-0_5 g-color-white--hover">
-								<i class="fa fa-instagram"></i>
-							</a>
-						</li>
-
-						<li class="list-inline-item g-mx-10" data-toggle="tooltip" data-placement="top"
-							title="Twitter">
-							<a href="https://twitter.com/rossedlin" target="_blank"
-							   class="g-color-white-opacity-0_5 g-color-white--hover">
-								<i class="fa fa-twitter"></i>
-							</a>
-						</li>
-
-						<li class="list-inline-item g-mx-10" data-toggle="tooltip" data-placement="top"
-							title="Linkedin">
-							<a href="https://www.linkedin.com/in/rossedlin/" target="_blank"
-							   class="g-color-white-opacity-0_5 g-color-white--hover">
-								<i class="fa fa-linkedin"></i>
-							</a>
-						</li>
-
-
-						<li class="list-inline-item g-mx-10" data-toggle="tooltip" data-placement="top"
-							title="GitHub">
-							<a href="https://github.com/rossedlin/" target="_blank"
-							   class="g-color-white-opacity-0_5 g-color-white--hover">
-								<i class="fa fa-github"></i>
-							</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</footer>
-	<!-- End Footer -->
-</main>
-
-<!-- JS Global Compulsory -->
-<script src="/assets/vendor/jquery/jquery.min.js"></script>
-<script src="/assets/vendor/jquery-migrate/jquery-migrate.min.js"></script>
-<script src="/assets/vendor/jquery.easing/js/jquery.easing.js"></script>
-<script src="/assets/vendor/popper.min.js"></script>
-<script src="/assets/vendor/bootstrap/bootstrap.min.js"></script>
-
-<!-- JS Implementing Plugins -->
-<script src="/assets/vendor/appear.js"></script>
-<script src="/assets/vendor/slick-carousel/slick/slick.js"></script>
-<script src="/assets/vendor/cubeportfolio-full/cubeportfolio/js/jquery.cubeportfolio.min.js"></script>
-<script src="/assets/vendor/dzsparallaxer/dzsparallaxer.js"></script>
-<script src="/assets/vendor/dzsparallaxer/dzsscroller/scroller.js"></script>
-<script src="/assets/vendor/dzsparallaxer/advancedscroller/plugin.js"></script>
-<script src="/assets/vendor/fancybox/jquery.fancybox.js"></script>
-
-<!-- JS Unify -->
-<script src="/assets/js/hs.core.js"></script>
-<script src="/assets/js/components/hs.header.js"></script>
-<script src="/assets/js/helpers/hs.hamburgers.js"></script>
-<script src="/assets/js/components/hs.scroll-nav.js"></script>
-<script src="/assets/js/components/hs.counter.js"></script>
-<script src="/assets/js/components/hs.carousel.js"></script>
-<script src="/assets/js/components/hs.cubeportfolio.js"></script>
-<script src="/assets/js/components/hs.popup.js"></script>
-<script src="/assets/js/components/hs.go-to.js"></script>
-
-<!-- JS Revolution Slider -->
-<script src="/assets/vendor/revolution-slider/revolution/js/jquery.themepunch.tools.min.js"></script>
-<script src="/assets/vendor/revolution-slider/revolution/js/jquery.themepunch.revolution.min.js"></script>
-
-<script
-	src="/assets/vendor/revolution-slider/revolution-addons/typewriter/js/revolution.addon.typewriter.min.js"></script>
-
-<script src="/assets/vendor/revolution-slider/revolution/js/extensions/revolution.extension.actions.min.js"></script>
-<script src="/assets/vendor/revolution-slider/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
-<script src="/assets/vendor/revolution-slider/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
-<script
-	src="/assets/vendor/revolution-slider/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
-<script src="/assets/vendor/revolution-slider/revolution/js/extensions/revolution.extension.migration.min.js"></script>
-<script src="/assets/vendor/revolution-slider/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
-<script src="/assets/vendor/revolution-slider/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
-<script src="/assets/vendor/revolution-slider/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
-<script src="/assets/vendor/revolution-slider/revolution/js/extensions/revolution.extension.video.min.js"></script>
-
-<!-- JS Customization -->
-<script src="/assets/js/custom.js"></script>
+        <div class="links">
+            <a href="http://projects.rossedlin.com/landing-page" target="_blank">Landing Page</a>
+            <a href="http://www.rossedlin.com/" target="_blank">Ross Edlin</a>
+            <a href="http://www.kinktank.co.uk/" target="_blank">KinkTank</a>
+        </div>
+    </div>
+</div>
 </body>
 </html>
